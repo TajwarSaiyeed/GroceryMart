f2373f86786399ed599a5741711a8ea6a7e5ca91
tajwarsaiyeed <rjabid36@gmail.com>
2025-02-06 14:45:21 +0600

    feat: Add requirements.txt for project dependencies and update CSRF_TRUSTED_ORIGINS in settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -5,7 +5,6 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env("SECRET_KEY")
@@ -13,7 +12,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', "http://127.0.0.1"]
 
 # Application definition
 
